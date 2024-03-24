e8e0781b48842bca2da7aebe2c605b2ef736d2fe
AlejandroYanes <alejandro.yanes94@gmail.com>
2024-03-24 17:41:56 +0000

    small update to the empty/error states for the tables

--- src/components/empty-state.jsx
+++ src/components/empty-state.jsx
@@ -1,6 +1,10 @@
 // eslint-disable-next-line import/no-unresolved
 import { Heading } from 'monday-ui-react-core/next';
 
-const TableEmptyState = () => <Heading style={{ textAlign: 'center', margin: '24px 0' }}>Empty State</Heading>;
+const TableEmptyState = () => (
+  <Heading style={{ textAlign: 'center', margin: '24px 0' }}>
+    No records found
+  </Heading>
+);
 
 export default TableEmptyState;
--- src/components/error-state.jsx
+++ src/components/error-state.jsx
@@ -1,6 +1,10 @@
 // eslint-disable-next-line import/no-unresolved
 import { Heading } from 'monday-ui-react-core/next';
 
-const TableErrorState = () => <Heading style={{ textAlign: 'center', margin: '24px 0' }}>Error State</Heading>;
+const TableErrorState = () => (
+  <Heading style={{ textAlign: 'center', margin: '24px 0' }}>
+    Something went wrong. Please try again later.
+  </Heading>
+);
 
 export default TableErrorState;
