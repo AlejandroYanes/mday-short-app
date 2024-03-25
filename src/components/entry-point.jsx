ee1b878ad9d7f8700e917ba9e54f0d9bb20726fe
AlejandroYanes <alejandro.yanes94@gmail.com>
2024-03-25 08:36:04 +0000

    moved the entry-point component to use a switch

--- src/components/entry-point.jsx
+++ src/components/entry-point.jsx
@@ -10,61 +10,54 @@ import MainView from './main-view';
 export default function EntryPoint() {
   const { status } = useAuth();
 
-  if (status === APP_STATUS.UNKNOWN) {
-    return (
-      <div className="app">
-        <div className="loading-screen">
-          <Heading align={Heading.align.CENTER}>Loading...</Heading>
+  switch (status) {
+    case APP_STATUS.UNKNOWN:
+      return (
+        <div className="app">
+          <div className="loading-screen">
+            <Heading align={Heading.align.CENTER}>Loading...</Heading>
+          </div>
         </div>
-      </div>
-    );
-  }
-
-  if (status === APP_STATUS.PENDING) {
-    return (
-      <div className="app">
-        <div className="fail-screen">
-          <Heading align={Heading.align.CENTER}>Hi there</Heading>
-          <Text type={Text.types.TEXT1} align={Text.align.CENTER}>
-            Your account is pending approval.
-            Please wait for an admin to approve your request.
-          </Text>
+      );
+    case APP_STATUS.PENDING:
+      return (
+        <div className="app">
+          <div className="fail-screen">
+            <Heading align={Heading.align.CENTER}>Hi there</Heading>
+            <Text type={Text.types.TEXT1} align={Text.align.CENTER}>
+              Your account is pending approval.
+              Please wait for an admin to approve your request.
+            </Text>
+          </div>
         </div>
-      </div>
-    );
-  }
-
-  if (status === APP_STATUS.INVITED) {
-    return (
-      <div className="app">
-        <div className="fail-screen">
-          <Heading align={Heading.align.CENTER}>Hi there</Heading>
-          <Text type={Text.types.TEXT1} align={Text.align.CENTER}>
-            You have been invited to join this workspace.
-            Please check your email for an invitation link.
-          </Text>
+      );
+    case APP_STATUS.INVITED:
+      return (
+        <div className="app">
+          <div className="fail-screen">
+            <Heading align={Heading.align.CENTER}>Hi there</Heading>
+            <Text type={Text.types.TEXT1} align={Text.align.CENTER}>
+              You have been invited to join this workspace.
+              Please check your email for an invitation link.
+            </Text>
+          </div>
         </div>
-      </div>
-    );
+      );
+    case APP_STATUS.NEEDS_SETUP:
+      return <SetupScreen/>;
+    case APP_STATUS.AUTHENTICATED:
+      return <MainView/>;
+    default:
+      return (
+        <div className="app">
+          <div className="fail-screen">
+            <Heading align={Heading.align.CENTER}>Error</Heading>
+            <Text type={Text.types.TEXT1} align={Text.align.CENTER}>
+              An error occurred while trying to authenticate you.
+              Please try again later.
+            </Text>
+          </div>
+        </div>
+      );
   }
-
-  if (status === APP_STATUS.NEEDS_SETUP) {
-    return <SetupScreen/>;
-  }
-
-  if (status === APP_STATUS.AUTHENTICATED) {
-    return <MainView/>;
-  }
-
-  return (
-    <div className="app">
-      <div className="fail-screen">
-        <Heading align={Heading.align.CENTER}>Error</Heading>
-        <Text type={Text.types.TEXT1} align={Text.align.CENTER}>
-          An error occurred while trying to authenticate you.
-          Please try again later.
-        </Text>
-      </div>
-    </div>
-  );
 }
